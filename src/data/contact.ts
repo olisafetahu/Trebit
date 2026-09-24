517744526ffaf5bec0f2209af91e9a6161ef7242
olisafetahu <olisafetahuu@gmail.com>
2026-09-24 15:02:37 +0200

    .

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,7 +2,7 @@ export const contact = {
   website: "www.kubit-ks.com",
   websiteUrl: "https://www.kubit-ks.com",
   email: "info@kubit-ks.com",
-  phone: "",
+  phone: "+383 48 702 707",
   social: {
     facebook: "",
     instagram: "",
